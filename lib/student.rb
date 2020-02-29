4b2fb3f44482f852021ddca170ce496c983aebf4
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-29 15:50:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,9 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  attr_accessor :id, :name, :grade
+self.column_names.each do |col_name|
+  attr_accessor col_name.to_sym
+end
 
-  def initialize(attributes)
-    @id = attributes[id]
-    @name = attributes[name]
-    @grade = attributes[grade]
-  end
+
 end
