ae2fe94d03bc92873039a1478a53d9d90758dfba
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-29 16:07:07 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -28,4 +28,11 @@ class InteractiveRecord
     self.class.table_name
   end
 
+  def col_names_for_insert
+    self.class.column_names each do |col_name|
+      values << "#{send(col_name)}" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
+
 end
