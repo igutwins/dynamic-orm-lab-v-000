db340590c0265197ce6b1da5fbeacbd88025fda6
Ian Gutwinski <iangutwinski@gmail.com>
2020-02-29 16:35:53 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -51,4 +51,9 @@ class InteractiveRecord
     DB[:conn].execute(sql)
   end
 
+  def self.find_by(attribute)
+    sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = '#{attribute}'"
+    DB[:conn].execute(sql)
+  end
+
 end
